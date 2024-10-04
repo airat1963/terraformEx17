f229d02e66d33902c3f5381549a0a80736ab1056
airat1963 <33879798+airat1963@users.noreply.github.com>
2024-10-04 12:11:44 +0300

    Update main.tf
    remove sensitive from main

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@
 # # Инициализация провайдера Yandex.Cloud
 # provider "yandex" {
 
-#   token     = "y0_AgAAAAAeR3RAAAxSDwAAAAEOrTZ_AABH2orPA0tO_Z8q1RPisnsS4sJdRg"
+
 #   cloud_id  = "b1gjkqhfee2fvrk4s12a"
 #   folder_id = "b1g77g5lrul51pldt28m"
 #   zone      = "ru-central1-a"
@@ -21,9 +21,9 @@
 resource "yandex_storage_bucket" "my_bucket" {
   bucket = "my-unique-bucket-name"
 
-  access_key = "YCAJEp3BihnkkJlWiSzygELcw"
+  access_key = "***"
 
-  secret_key = "YCPV7I-i1lX7rzHPn6IG8-jUfeXeqNAe4dr8Egcb"
+  secret_key = "***"
 
 }
 
