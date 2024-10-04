12870ade92fb86f04b7a6d57e18d67062acf37be
airat1963 <airat1963@gmail.com>
2024-10-04 11:56:26 +0300

    Remove sensitive information

--- provider.tf
+++ provider.tf
@@ -22,13 +22,3 @@ provider "yandex" {
 #   folder_id="b1g77g5lrul51pldt28m"
 #   cloud_id="b1gjkqhfee2fvrk4s12a"
 
-# }
-#  provider "yandex" {
-# #   zone      = var.zone
-#    folder_id = local.folder_id
-#    cloud_id = local.cloud_id
-# #  token     = var.token
-# #  token ="t1.9euelZqLyc2TmZeelMyJnpiVjMyXnu3rnpWaxpuPk5OJm4-Ni8fJx5nOkMvl8_cqShFI-e9-aUFU_N3z92p4Dkj5735pQVT8zef1656Vmo-clJ3HkcvLl5vOyZGWnJHK7_zF656Vmo-clJ3HkcvLl5vOyZGWnJHK.L-Xk_N9pAXx7vmis8VBdswhr6NVuzPBMyJZjWswUd1lwr1Lew0d5tuLq4_Pn5svtYsWBuZEj1Q8H7C2s9fp2CQ"
-#    service_account_key_file = "C:/Users/univer/terraform4bucket/keyIAM.json"
-
-#  }
